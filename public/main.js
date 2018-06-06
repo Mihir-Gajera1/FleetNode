d01b39a0d35b898d10de41a039dc5c3db56acb95
Mihir <mihirgajera1@gmail.com>
2018-06-06 15:18:40 +0530

    welcome

--- public/main.js
+++ public/main.js
@@ -7,18 +7,19 @@ const otpbutton = document.getElementById('getotp');
 const signupbutton  = document.getElementById('signup');
 otpbutton.addEventListener('click',otp);
 signupbutton.addEventListener('click',signup);
-//button.addEventListener('click',otp,false);
 
 function otp() {
   console.log('SENDING OTP');
   const number = phoneNumber.value.replace(/\D/g, '');
   const text = "OTP";
-  fetch('/', {
+  console.log(number);
+  fetch('/register', {
     method: 'post',
     headers: {
       'Content-type': 'application/json'
     },
-    body: JSON.stringify({number: number,text: text})
+
+    body: JSON.stringify({number: number,type: text})
   })
   .then(function(res){
     console.log(res);
@@ -37,15 +38,15 @@ function signup(){
   const rpswd = password2.value.replace(/\D/g, '');
   const otpnum = otpNumber.value.replace(/\D/g, '');
   const text = "SIGNUP";
-  fetch('/', {
+  fetch('/register', {
     method: 'post',
     headers: {
       'Content-type': 'application/json'
     },
-    body: JSON.stringify({companyname: companyname,pswd:pswd,rpswd:rpswd ,pnum: phonenumber ,otpnum: otpnum,text:text})
+    body: JSON.stringify({companyname: companyname,pswd:pswd,rpswd:rpswd ,pnum: phonenumber ,otpnum: otpnum,type:text})
   })
   .then(function(res){
-    console.log(res);
+    console.log(res.msg);
   })
   .catch(function(err){
     console.log(err);
--- signup.js
+++ signup.js
@@ -1,29 +1,37 @@
-$(document).ready(function(){
+var SendOtp = require('sendotp');
+var sendOtp = new SendOtp('217185ASckRx215b07feb6');
 
-  $('otp').on('click', function(){
+module.exports = function(app){
 
-      var item = $('phonenumber');
-      var todo = {item: item.val()};
+app.get('/register',function(req,res){
+   
+         res.render(__dirname + '/public/index.html');
+});
+app.post('/register', (req, res) => {
+ 
+		const type = req.body.type;
+ 		if("OTP"===type)
+ 		{
+ 			const number = req.body.number;
+			var contactNumber = "+91"+String(number);
+			sendOtp.send(contactNumber,"FLEETTRACK",function (error, data, response) {
+  			console.log(data);
+			});
+			sendOtp.setOtpExpiry('5');
+		}
+		if("SIGNUP"==type)
+		{
+			const number = req.body.pnum;
+			var contactNumber = "+91"+String(number);
+			var otp  = req.body.otpnum;
+			var otpforver = String(otp);
+			sendOtp.verify(contactNumber,otpforver, function (error, data, response) {
+  			console.log(data);
+ 			 if(data.type == 'success') {
+  			console.log('OTP verified successfully');}
+  			if(data.type == 'error') console.log('OTP verification failed')});
+  		}
 
-      $.ajax({
-        type: 'GET',
-        url: '/index.html',
-        data: todo,
-        success: function(data){
-          //do something with the data via front-end framework.
-
-
-
-  });
 });
 
-
-/*module.exports = {
-    getotp: function() {var contactNumber = "+91"+String(document.getElementById('phonenumber').value);
-sendOtp.send(contactNumber,"FLEETTRACK",function (error, data, response) {
-  console.log(data);
-sendOtp.setOtpExpiry('5');});
-},
-
-    signup: function() {}
-}*/
+}
